950a0cdbbc4648db7ef0e8092720cf1a9c29180a
Ayoub ABOUNAKIF <ayoubabounakif@gmail.com>
2023-12-12 19:54:17 +0100

    Add logistic regression setup code and notebooks

---
+++ logistic_regression/logreg.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class LogisticRegression:
+  def __init__(self):
+    pass
---
+++ logistic_regression/logreg_predict.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
+def main():
+  # TODO: Take dataset as parameter
+  # TODO: Implement on Notebook
+  pass
+
+
+if __name__ == '__main__':
+  main()
